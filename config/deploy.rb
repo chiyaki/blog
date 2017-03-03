894971107baea49404dae614b5cf7675687ac7b5
kay <kaywangfan@163.com>
2017-03-03 19:09:07 +0800

    Add capistrano file

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+# config valid only for current version of Capistrano
+lock "3.7.1"
+
+set :application, "blog"
+set :repo_url, "git@github.com:chiyaki/blog.git"
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/apps/blog"
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
